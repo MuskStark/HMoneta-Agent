a5dc278703d8f3b4e71043d42b278ae36c855010
MuskStark <gitman@outlook.sg>
2024-05-19 22:24:23 +0800

    feat: 新增启动banner

--- src/main/java/fan/summer/agent/HMonetaAgentApplication.java
+++ src/main/java/fan/summer/agent/HMonetaAgentApplication.java
@@ -1,5 +1,6 @@
 package fan.summer.agent;
 
+import fan.summer.agent.common.ProjectBanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -17,6 +18,7 @@ public class HMonetaAgentApplication {
     public static void main(String[] args) {
         MDC.put("LOG_ID", String.valueOf(System.currentTimeMillis()));
         SpringApplication application = new SpringApplication(HMonetaAgentApplication.class);
+        application.setBanner(new ProjectBanner());
         Environment env = application.run(args).getEnvironment();
         LOG.info("服务启动成功!");
         LOG.info("启动成功，项目地址：http://localhost:{}", env.getProperty("server.port"));
